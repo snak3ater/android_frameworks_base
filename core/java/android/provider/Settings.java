9f98004df41fcfe609848b71a81afe20dc53ca63
Lalit Maganti <lalitmaganti@gmail.com>
2013-11-26 18:51:16 +0000

    Bring back the status bar activity indicators for mobile data and wifi
    * adds a setting for this as well as reverting the following commit:
    
    Reverts "Remove signal/wifi activity indicator icon overlays."
    
    This reverts commit fcc901c10bffdfb9c6b31e676dec36b47d949979.
    
    Change-Id: I7aa85f7c8442c45d17e42cac7823dfe93e3f24c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3077,6 +3077,12 @@ public final class Settings {
         public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
 
         /**
+         * Show when WiFi or data mobile is sending/receiving data
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_activity";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
