4c37977b440115ee400335f115d385b630f2e280
rascarlo <rascarlo@gmail.com>
2013-11-30 13:44:49 +0100

    Quick launch powerMenu action
    Conflicts:
    	core/java/com/android/internal/util/cm/NavigationRingConstants.java
    	core/res/res/values/ras_symbols.xml

--- packages/SystemUI/src/com/android/systemui/cm/ActionTarget.java
+++ packages/SystemUI/src/com/android/systemui/cm/ActionTarget.java
@@ -191,6 +191,10 @@ public class ActionTarget {
 	    intent.putExtra(AUTO_START, true);
             mContext.sendBroadcast(intent);
             return true;
+        } else if (action.equals(ACTION_POWER_MENU)) {
+            Intent intent = new Intent(Intent.ACTION_POWERMENU);
+                mContext.sendBroadcast(intent);
+            return true;
         } else {
             try {
                 Intent intent = Intent.parseUri(action, 0);
