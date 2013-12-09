de85f2d87bf3ac39b7fe2a38055783e312917a31
lichti <lichti@lichti>
2013-12-09 22:03:39 +0100

    Revert "Remove dead code"
    This reverts commit 08c0283b0fd88e6820e00590e6250301fcc65c1a.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -280,6 +280,10 @@ public class NavigationBarView extends LinearLayout {
         return mCurrentView;
     }
 
+    public View getRecentsButton() {
+        return mCurrentView.findViewById(R.id.recent_apps);
+    }
+
     public boolean isInEditMode() {
         return mInEditMode;
     }
