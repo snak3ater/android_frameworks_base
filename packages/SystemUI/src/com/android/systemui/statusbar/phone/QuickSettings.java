5650e213437c60e2df2a0ade75d6f2e7af4a32a9
David96 <xd_e-mail@gmx.de>
2014-01-02 16:45:33 +0100

    Fix NFC tile not showing on reboot
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -33,6 +33,7 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -46,7 +47,6 @@ import android.media.MediaRouter;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.nfc.NfcAdapter;
-import android.nfc.NfcManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.PowerManager;
@@ -140,7 +140,6 @@ class QuickSettings {
     private BluetoothController mBluetoothController;
     private RotationLockController mRotationLockController;
     private LocationController mLocationController;
-    private NfcAdapter mNfcAdapter;
 
     private AsyncTask<Void, Void, Pair<String, Drawable>> mUserInfoTask;
     private AsyncTask<Void, Void, Pair<Boolean, Boolean>> mQueryCertTask;
@@ -165,8 +164,6 @@ class QuickSettings {
         mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         mConnectivityManager =
                     (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NfcManager nfcManager = (NfcManager) mContext.getSystemService(Context.NFC_SERVICE);
-        mNfcAdapter = nfcManager.getDefaultAdapter();
 
         mHandler = new Handler();
 
@@ -375,6 +372,10 @@ class QuickSettings {
         return selection == 1 || selection == 2;
     }
 
+    private boolean isNfcSupported() {
+        return mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC);
+    }
+    
     private void addTiles(ViewGroup parent, boolean addMissing) {
         // Load all the customizable tiles. If not yet modified by the user, load default ones.
         // After enabled tiles are loaded, proceed to load missing tiles and set them to View.GONE.
@@ -973,10 +974,11 @@ class QuickSettings {
                     });
                     parent.addView(immersiveTile);
                     if(addMissing) immersiveTile.setVisibility(View.GONE);
-               } else if(Tile.NFC.toString().equals(tile.toString()) && mNfcAdapter != null) {
+                } else if(Tile.NFC.toString().equals(tile.toString()) && isNfcSupported()) {
                     final QuickSettingsBasicTile nfcTile = new QuickSettingsBasicTile(mContext);
                     nfcTile.setTileId(Tile.NFC);
-                    boolean nfcEnabled = mNfcAdapter.isEnabled();
+                    NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
+                    boolean nfcEnabled = nfcAdapter != null ? nfcAdapter.isEnabled() : false;
                     nfcTile.setImageResource(nfcEnabled ?
                             R.drawable.ic_qs_nfc_on :
                             R.drawable.ic_qs_nfc_off);
@@ -987,19 +989,24 @@ class QuickSettings {
 
                         @Override
                         public void onClick(View v) {
-                            boolean enabled = mNfcAdapter.isEnabled();
-                            if (enabled) {
-                                mNfcAdapter.disable();
-                            } else {
-                                mNfcAdapter.enable();
+                            //We have to use that deprecated function here, 
+                            //because getDefaultAdapter(mContext) always returns null with the context we get on boot
+                            NfcAdapter adapter = NfcAdapter.getDefaultAdapter();
+                            if (adapter != null) {
+                                boolean enabled = adapter.isEnabled();
+                                if (enabled) {
+                                    adapter.disable();
+                                } else {
+                                    adapter.enable();
+                                }
+                                enabled = !enabled;
+                                nfcTile.setImageResource(enabled ?
+                                        R.drawable.ic_qs_nfc_on :
+                                        R.drawable.ic_qs_nfc_off);
+                                nfcTile.setTextResource(enabled ?
+                                        R.string.quick_settings_nfc_label :
+                                        R.string.quick_settings_nfc_off_label);
                             }
-                            enabled = !enabled;
-                            nfcTile.setImageResource(enabled ?
-                                    R.drawable.ic_qs_nfc_on :
-                                    R.drawable.ic_qs_nfc_off);
-                            nfcTile.setTextResource(enabled ?
-                                    R.string.quick_settings_nfc_label :
-                                    R.string.quick_settings_nfc_off_label);
                         }
                     });
                     mModel.addNfcTile(nfcTile, new QuickSettingsModel.BasicRefreshCallback(nfcTile));
