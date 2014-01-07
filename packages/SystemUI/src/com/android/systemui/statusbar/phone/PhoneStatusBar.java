d9118ac1fdafcf6a2c17b723d6f9d7e602e78395
Danesh M <daneshm90@gmail.com>
2014-01-07 04:23:03 -0500

    [1/2] NavigationBar Customization
    - Allows user to customize navigation bar.
      Features :
       * Choose from 1-6 # of buttons
       * Re-arrange / assign buttons
    
    - Navbar : Rework logic for determining orientation
    
    PS2: Add DeviceUtils helper class from slimroms
    
    Change-Id: Ifea7efdfe1374e71e617a113624c265938a524aa
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout-ldrtl/navigation_bar.xml
    	packages/SystemUI/res/layout/navigation_bar.xml
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    
    Conflicts:
    	core/java/com/android/internal/util/paranoid/DeviceUtils.java
    	packages/SystemUI/res/values/pa_strings.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -55,6 +55,7 @@ import android.graphics.drawable.Drawable;
 import android.inputmethodservice.InputMethodService;
 import android.net.Uri;
 import android.os.Bundle;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.IPowerManager;
@@ -1024,10 +1025,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
 
-        mNavigationBarView.getRecentsButton().setOnClickListener(mRecentsClickListener);
-        mNavigationBarView.getRecentsButton().setOnTouchListener(mRecentsPreloadOnTouchListener);
-        mNavigationBarView.getHomeButton().setOnTouchListener(mHomeSearchActionListener);
-        mNavigationBarView.getSearchLight().setOnTouchListener(mHomeSearchActionListener);
+        mNavigationBarView.setListeners(mRecentsClickListener,
+                mRecentsPreloadOnTouchListener, mHomeSearchActionListener);
         updateSearchPanel();
     }
 
@@ -3075,6 +3074,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         //updateNotificationIcons();
         //updateResources();
         recreateStatusBar();
+        if (mNavigationBarView != null) {
+            mNavigationBarView.updateSettings();
+        }
+        super.userSwitched(newUserId);
     }
 
     private void resetUserSetupObserver() {
@@ -3331,6 +3334,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     protected boolean shouldDisableNavbarGestures() {
         return !isDeviceProvisioned()
                 || mExpandedVisible
+                || (mNavigationBarView != null && mNavigationBarView.isInEditMode())
                 || (mDisabled & StatusBarManager.DISABLE_SEARCH) != 0;
     }
 
