af57c8af6aa6c6ed6c58cb1a06862a29a2a6971f
lichti <lichti@lichti>
2014-02-02 10:47:37 +0100

    Revert "[2/2] Framework AppOps: Forward port ops from cm-10.2"
    This reverts commit 1dcc70315fff5476a7da49e1aa2854bc0e9b5ef9.

--- services/java/com/android/server/AlarmManagerService.java
+++ services/java/com/android/server/AlarmManagerService.java
@@ -19,7 +19,6 @@ package com.android.server;
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.AlarmManager;
-import android.app.AppOpsManager;
 import android.app.IAlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -113,8 +112,6 @@ class AlarmManagerService extends IAlarmManager.Stub {
     private final ResultReceiver mResultReceiver = new ResultReceiver();
     private final PendingIntent mTimeTickSender;
     private final PendingIntent mDateChangeSender;
-
-    private final AppOpsManager mAppOps;
 
     class WakeupEvent {
         public long when;
@@ -401,7 +398,7 @@ class AlarmManagerService extends IAlarmManager.Stub {
                             : maxTriggerTime(nowElapsed, whenElapsed, a.repeatInterval);
                 }
                 setImplLocked(a.type, a.when, whenElapsed, a.windowLength, maxElapsed,
-                        a.repeatInterval, a.operation, batch.standalone, doValidate, a.workSource, false);
+                        a.repeatInterval, a.operation, batch.standalone, doValidate, a.workSource);
             }
         }
     }
@@ -501,8 +498,6 @@ class AlarmManagerService extends IAlarmManager.Stub {
         } else {
             Slog.w(TAG, "Failed to open alarm driver. Falling back to a handler.");
         }
-
-        mAppOps = (AppOpsManager)mContext.getSystemService(Context.APP_OPS_SERVICE);
     }
     
     protected void finalize() throws Throwable {
@@ -563,23 +558,6 @@ class AlarmManagerService extends IAlarmManager.Stub {
             maxElapsed = triggerElapsed + windowLength;
         }
 
-        boolean wakeupFiltered = false;
-        if ((type == AlarmManager.RTC_WAKEUP
-                        || type == AlarmManager.ELAPSED_REALTIME_WAKEUP)
-                && mAppOps.checkOpNoThrow(AppOpsManager.OP_ALARM_WAKEUP,
-                        operation.getCreatorUid(),
-                        operation.getCreatorPackage())
-                != AppOpsManager.MODE_ALLOWED) {
-
-            if (type == AlarmManager.RTC_WAKEUP) {
-                type = AlarmManager.RTC;
-            } else {
-                type = AlarmManager.ELAPSED_REALTIME;
-            }
-
-            wakeupFiltered = true;
-        }
-
         synchronized (mLock) {
             if (DEBUG_BATCH) {
                 Slog.v(TAG, "set(" + operation + ") : type=" + type
@@ -588,26 +566,16 @@ class AlarmManagerService extends IAlarmManager.Stub {
                         + " interval=" + interval + " standalone=" + isStandalone);
             }
             setImplLocked(type, triggerAtTime, triggerElapsed, windowLength, maxElapsed,
-                    interval, operation, isStandalone, true, workSource, wakeupFiltered);
+                    interval, operation, isStandalone, true, workSource);
         }
-   }
+    }
 
     private void setImplLocked(int type, long when, long whenElapsed, long windowLength,
             long maxWhen, long interval, PendingIntent operation, boolean isStandalone,
-            boolean doValidate, WorkSource workSource, boolean wakeupFiltered) {
+            boolean doValidate, WorkSource workSource) {
         Alarm a = new Alarm(type, when, whenElapsed, windowLength, maxWhen, interval,
                 operation, workSource);
-
-        // Remove this alarm if already scheduled.
-        final boolean foundExistingWakeup = removeWithReportLocked(operation);
-
-        // note AppOp for accounting purposes
-        // skip if the alarm already existed
-        if (!foundExistingWakeup && wakeupFiltered) {
-            mAppOps.noteOpNoThrow(AppOpsManager.OP_ALARM_WAKEUP,
-                    operation.getCreatorUid(),
-                    operation.getCreatorPackage());
-        }
+        removeLocked(operation);
 
         int whichBatch = (isStandalone) ? -1 : attemptCoalesceLocked(whenElapsed, maxWhen);
         if (whichBatch < 0) {
@@ -760,10 +728,6 @@ class AlarmManagerService extends IAlarmManager.Stub {
     }
     
     public void removeLocked(PendingIntent operation) {
-        removeWithReportLocked(operation);
-    }
-
-    private boolean removeWithReportLocked(PendingIntent operation) {
         boolean didRemove = false;
         for (int i = mAlarmBatches.size() - 1; i >= 0; i--) {
             Batch b = mAlarmBatches.get(i);
@@ -780,8 +744,6 @@ class AlarmManagerService extends IAlarmManager.Stub {
             rebatchAllAlarmsLocked(true);
             rescheduleKernelAlarmsLocked();
         }
-
-        return didRemove;
     }
 
     public void removeLocked(String packageName) {
@@ -1092,7 +1054,7 @@ class AlarmManagerService extends IAlarmManager.Stub {
                     setImplLocked(alarm.type, alarm.when + delta, nextElapsed, alarm.windowLength,
                             maxTriggerTime(nowELAPSED, nextElapsed, alarm.repeatInterval),
                             alarm.repeatInterval, alarm.operation, batch.standalone, true,
-                            alarm.workSource, false);
+                            alarm.workSource);
                 }
 
             }
@@ -1281,10 +1243,6 @@ class AlarmManagerService extends IAlarmManager.Stub {
                                 fs.numWakeup++;
                                 ActivityManagerNative.noteWakeupAlarm(
                                         alarm.operation);
-                                // AppOps accounting
-                                mAppOps.noteOpNoThrow(AppOpsManager.OP_ALARM_WAKEUP,
-                                        alarm.operation.getCreatorUid(),
-                                        alarm.operation.getCreatorPackage());
                             }
                         } catch (PendingIntent.CanceledException e) {
                             if (alarm.repeatInterval > 0) {
--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -1011,7 +1011,7 @@ public class WifiManager {
      */
     public boolean setWifiEnabled(boolean enabled) {
         try {
-            return mService.setWifiEnabled(mContext.getBasePackageName(), enabled);
+            return mService.setWifiEnabled(enabled);
         } catch (RemoteException e) {
             return false;
         }
