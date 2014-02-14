6360fa247ee3059299496c2fed3d5b40959b63cb
lichti <lichti@lichti>
2014-02-14 21:51:39 +0100

    Revert "Revert "Revert "initialize mContext -- fix"""
    This reverts commit c84c76aff5626d1bb6f487471c657759ad3a88fe.

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -382,7 +382,6 @@ final class DisplayPowerController {
             DisplayManagerService displayManager,
             SuspendBlocker displaySuspendBlocker, DisplayBlanker displayBlanker,
             Callbacks callbacks, Handler callbackHandler) {
-        mContext = context;
         mHandler = new DisplayControllerHandler(looper);
         mNotifier = notifier;
         mDisplaySuspendBlocker = displaySuspendBlocker;
