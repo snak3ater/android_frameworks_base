72166b378d1fc06c87b71ee2615e47ac0317825c
lichti <lichti@lichti>
2014-02-19 22:04:58 +0100

    Revert "[2/2] Framework/base: Lockscreen maximized widgets"
    This reverts commit 4fcaea886d8f54df55c91fabe8a2e3569bec4c85.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2711,13 +2711,6 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
-         * Weather to minimize lockscreen challenge on screen turned on
-         * @hide
-         */
-        public static final String LOCKSCREEN_MAXIMIZE_WIDGETS = "lockscreen_maximize_widgets";
-
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
@@ -85,7 +85,6 @@ public class KeyguardHostView extends KeyguardViewBase {
     private KeyguardSecurityViewFlipper mSecurityViewContainer;
     private KeyguardSelectorView mKeyguardSelectorView;
     private KeyguardTransportControlView mTransportControl;
-    private View mExpandChallengeView;
     private boolean mIsVerifyUnlockOnly;
     private boolean mEnableFallback; // TODO: This should get the value from KeyguardPatternView
     private SecurityMode mCurrentSecuritySelection = SecurityMode.Invalid;
@@ -399,22 +398,7 @@ public class KeyguardHostView extends KeyguardViewBase {
         showPrimarySecurityScreen(false);
         updateSecurityViews();
         enableUserSelectorIfNecessary();
-        minimizeChallengeIfDesired();
-
-        mExpandChallengeView = (View) findViewById(R.id.expand_challenge_handle);
-        if (mExpandChallengeView != null) {
-            mExpandChallengeView.setOnLongClickListener(mFastUnlockClickListener);
-            mExpandChallengeView.bringToFront();
-        }
     }
-
-    private final OnLongClickListener mFastUnlockClickListener = new OnLongClickListener() {
-        @Override
-        public boolean onLongClick(View v) {
-            showNextSecurityScreenOrFinish(false);
-            return true;
-        }
-    };
 
     private void updateAndAddWidgets() {
         cleanupAppWidgetIds();
@@ -1061,7 +1045,6 @@ public class KeyguardHostView extends KeyguardViewBase {
         }
 
         requestFocus();
-        minimizeChallengeIfDesired();
     }
 
     @Override
@@ -1115,19 +1098,6 @@ public class KeyguardHostView extends KeyguardViewBase {
             // otherwise, go to the unlock screen, see if they can verify it
             mIsVerifyUnlockOnly = true;
             showSecurityScreen(securityMode);
-        }
-    }
-
-    private void minimizeChallengeIfDesired() {
-        if (mSlidingChallengeLayout == null) {
-            return;
-        }
-
-        int setting = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS, 0, UserHandle.USER_CURRENT);
-
-        if (setting == 1) {
-            mSlidingChallengeLayout.showChallenge(false);
         }
     }
 
