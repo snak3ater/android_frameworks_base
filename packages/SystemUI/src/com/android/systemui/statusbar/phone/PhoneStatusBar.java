0242171125ef90e44ac4ca2aec9fcd26fbc9ff1d
Clark Scheff <clark@cyngn.com>
2014-04-25 18:29:54 -0700

    Reduce memory leaks by removing all views.
    Change-Id: I64b95981425840d101c8122421281976d0251bae

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3145,6 +3145,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             iconSlots.add(iconView.getStatusBarSlot());
         }
 
+        removeAllViews(mStatusBarWindow);
+
         // extract notifications.
         int nNotifs = mNotificationData.size();
         ArrayList<Pair<IBinder, StatusBarNotification>> notifications =
@@ -3175,6 +3177,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         updateExpandedViewPos(EXPANDED_LEAVE_ALONE);
         checkBarModes();
         mRecreating = false;
+    }
+
+    private void removeAllViews(ViewGroup parent) {
+        int N = parent.getChildCount();
+        for (int i = 0; i < N; i++) {
+            View child = parent.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                removeAllViews((ViewGroup) child);
+            }
+        }
+        parent.removeAllViews();
     }
 
     /**
