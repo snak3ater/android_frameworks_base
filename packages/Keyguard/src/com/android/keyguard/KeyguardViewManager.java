690383168b6b43d001438b952adb2ba4cdb1c6f7
Cristiano Matos <cristianobmatos@gmail.com>
2014-05-10 15:45:43 -0300

    [Base] Disbale LN by default
    Change-Id: I9f0054dbcd5a7ba4f7b6b6ec903fbdfd86df17a8

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -103,7 +103,7 @@ public class KeyguardViewManager {
 
     private NotificationHostView mNotificationView;
     private NotificationViewManager mNotificationViewManager;
-    private boolean mLockscreenNotifications = true;
+    private boolean mLockscreenNotifications = false;
 
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
