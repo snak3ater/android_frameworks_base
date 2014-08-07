673d62ecf66e672000f5e3cc7ee9531f324a5c15
BigBrother1984 <stevewatersy@gmail.com>
2014-08-07 15:43:09 +0200

    HOVER: A few improvements
    - Better touch outside touch input handling
    - On touch outside hide both case (expanded and non) in 1.25 secs
    - Tweak clear foreground notification when we are on the same app
    - Code style
    - Tiny cleanup
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: Ibb0c7c09ae3256bbf744f359cca651c8cb8112d9

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
@@ -585,7 +585,6 @@ public class Hover {
             public void onAnimationEnd(Animator animation) {
                 mLastNotification = nextNotification; // backup current notification object
                 setAnimatingVisibility(false);
-                setTouchOutside(false);
                 // check if there are other notif to override with
                 processOverridingQueue(isExpanded());
             }
@@ -635,7 +634,7 @@ public class Hover {
                             }
 
                             mHiding = false;
-                            setTouchOutside(false);
+                            setTouchOutside(false); // reset
 
                             // check if there are other notif to show
                             if (!quit && !mVolumePanelShowing) {
@@ -926,7 +925,8 @@ public class Hover {
     public void clearForegroundAppNotifications() {
         for (int i = 0; i < mNotificationList.size(); i++) {
             if (mNotificationList.get(i).getContent().getPackageName()
-                    .equals(mNotificationHelper.getForegroundPackageName())) {
+                    .equals(mNotificationHelper.getForegroundPackageName())
+                            && i > 0) {
                 mNotificationList.remove(i);
             }
         }
