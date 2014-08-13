1e944e206d8976f8ab0c203c14a45e2a0a0e8bc3
liquid0624 <rjbirnesser@gmail.com>
2014-08-13 19:57:05 -0400

    Frameworks: network traffic color option (1/2)
    Change-Id: I94f28e12cffb46fb36d2b311319bdbd7213d4d7c
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2978,6 +2978,12 @@ public final class Settings {
 	*/
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
+	/**
+         * Network stats Color style
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_COLOR = "network_traffic_color";
+
         /**
          * Whether or not to hide the network traffic indicator when there is no activity
          * @hide
@@ -3171,7 +3177,7 @@ public final class Settings {
          *
          * @hide
          */
-        public static final String HOVER_EXCLUDE_LOW_PRIORITY = "hover_exclude_low_priority";
+	public static final String HOVER_EXCLUDE_LOW_PRIORITY = "hover_exclude_low_priority";        
 
         /**
          * Hover: Only show up if StatusBar is hidden, default is 1 (on).
