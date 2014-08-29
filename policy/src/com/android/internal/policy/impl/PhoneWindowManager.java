9ef0cb3e139a284b85b688d2b17ae7c2bb1e2f31
Brian Larsen <brian@chezbel.com>
2014-08-29 01:12:09 -0400

    Frameworks: BootMessage: Squashed Commits:
    BootMessage: Only display package name during optimization.
    The previous implementation caused the package name to show up for
    other messages such as "Starting apps." or "Finishing boot.". Now
    we check if the base message starts with "Optimizing" and only
    display the package name if that is true.
    
    BootMessage: Show even more info in boot dialog
    Originally the boot message was: Optimizing app N of M
    In http://gerrit.aicp-rom.com/#/c/1779/ this was changed to show only the package name
    This change shows both of those messages on 2 lines.
    
    Change-Id: I346129124b1167fa3b5ca9d12b617ed045b58a2f

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5329,6 +5329,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mBootMsgDialog.setCancelable(false);
                     mBootMsgDialog.show();
                 }
+
+                if (!mBootMsgDialog.isShowing())
+                     mBootMsgDialog.show();
                 mBootMsgDialog.setMessage(msg);
                 if (DEBUG_BOOTMSG) Log.d(TAG, "********** showBootMessage(" + msg +", " + always + ") updated ***********");
                 if (currentPackageName != null) {
