omnirom/android_frameworks_base_old

[1/3] Base: QuietHours: allow disable system sound
taken from slimroms

https://gerrit.slimroms.net/#/c/12180/

Allow to disable :
1. DialPad tone
2. touch sounds
3. screen lock sound
4. volume adjust sound
5. dock sound

in quiethour time

Change-Id: I2a1a7949ac6bdc48376980fac8ad2ae2c90fdfc9

core/java/android/view/VolumePanel.java
@@ -46,6 +46,8 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
+import com.android.internal.util.slim.QuietHoursHelper;
+
 import java.util.HashMap;
 
 /**
@@ -849,6 +851,10 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         // If preference is no sound - just exit here
         if (Settings.System.getInt(mContext.getContentResolver(),
                  Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) == 0) {
+             return;
+        }
+
+        if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
              return;
         }
 

media/java/android/media/AudioManager.java
@@ -42,7 +42,9 @@ import android.view.KeyEvent;
 import android.view.VolumePanel;
 import android.view.Surface;
 import android.view.WindowManager;
+
 import com.android.internal.util.omni.DeviceUtils;
+import com.android.internal.util.slim.QuietHoursHelper;
 
 import java.util.HashMap;
 
@@ -1809,7 +1811,10 @@ public class AudioManager {
      * Settings has an in memory cache, so this is fast.
      */
     private boolean querySoundEffectsEnabled() {
-        return Settings.System.getInt(mContext.getContentResolver(), Settings.System.SOUND_EFFECTS_ENABLED, 0) != 0;
+        boolean soundEffectEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                           Settings.System.SOUND_EFFECTS_ENABLED, 0) != 0;
+        boolean inQuietHour = QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM);
+        return soundEffectEnabled && !inQuietHour;
     }
 
 

packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -57,7 +57,7 @@ import android.view.WindowManagerPolicy;
 
 import com.android.internal.telephony.IccCardConstants;
 import com.android.internal.widget.LockPatternUtils;
-
+import com.android.internal.util.slim.QuietHoursHelper;
 
 /**
  * Mediates requests related to the keyguard.  This includes queries about the
@@ -1232,6 +1232,10 @@ public class KeyguardViewMediator {
 
         if (mSuppressNextLockSound) {
             mSuppressNextLockSound = false;
+            return;
+        }
+
+        if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
             return;
         }
 

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -44,6 +44,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Process;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -55,6 +56,8 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+
+import com.android.internal.util.slim.QuietHoursHelper;
 
 import com.android.systemui.R;
 import com.android.systemui.screenshot.TrashScreenshot;
@@ -539,7 +542,9 @@ class GlobalScreenshot {
             @Override
             public void run() {
                 // Play the shutter sound to notify that we've taken a screenshot
-                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                if (!QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                }
 
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotView.buildLayer();

services/java/com/android/server/DockObserver.java
@@ -36,6 +36,8 @@ import android.util.Slog;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import com.android.internal.util.slim.QuietHoursHelper;
+
 /**
  * <p>DockObserver monitors for a docking station.
  */
@@ -147,8 +149,9 @@ final class DockObserver extends UEventObserver {
 
             // Play a sound to provide feedback to confirm dock connection.
             // Particularly useful for flaky contact pins...
-            if (Settings.Global.getInt(cr,
-                    Settings.Global.DOCK_SOUNDS_ENABLED, 1) == 1) {
+            if ((Settings.Global.getInt(cr,
+                    Settings.Global.DOCK_SOUNDS_ENABLED, 1) == 1)
+                   && !QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
                 String whichSound = null;
                 if (mDockState == Intent.EXTRA_DOCK_STATE_UNDOCKED) {
                     if ((mPreviousDockState == Intent.EXTRA_DOCK_STATE_DESK) ||

snak3ater/android_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	media/java/android/media/AudioManager.java

core/java/android/view/VolumePanel.java
@@ -49,6 +49,8 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
+import com.android.internal.util.simpleaosp.QuietHoursHelper;
+
 import java.util.HashMap;
 
 /**
@@ -829,6 +831,10 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         // If preference is no sound - just exit here
         if (Settings.System.getInt(mContext.getContentResolver(),
                  Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) == 0) {
+             return;
+        }
+
+        if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
              return;
         }
 

media/java/android/media/AudioManager.java
@@ -41,6 +41,8 @@ import android.view.Surface;
 import android.view.VolumePanel;
 import android.view.WindowManager;
 
+import com.android.internal.util.simpleaosp.QuietHoursHelper;
+
 import java.util.HashMap;
 
 /**
@@ -1797,7 +1799,10 @@ public class AudioManager {
      * Settings has an in memory cache, so this is fast.
      */
     private boolean querySoundEffectsEnabled() {
-        return Settings.System.getInt(mContext.getContentResolver(), Settings.System.SOUND_EFFECTS_ENABLED, 0) != 0;
+        boolean soundEffectEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                           Settings.System.SOUND_EFFECTS_ENABLED, 0) != 0;
+        boolean inQuietHour = QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM);
+        return soundEffectEnabled && !inQuietHour;
     }
 
 

packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -56,7 +56,7 @@ import android.view.WindowManagerPolicy;
 
 import com.android.internal.telephony.IccCardConstants;
 import com.android.internal.widget.LockPatternUtils;
-
+import com.android.internal.util.simpleaosp.QuietHoursHelper;
 
 /**
  * Mediates requests related to the keyguard.  This includes queries about the
@@ -1184,6 +1184,10 @@ public class KeyguardViewMediator {
 
         if (mSuppressNextLockSound) {
             mSuppressNextLockSound = false;
+            return;
+        }
+
+        if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
             return;
         }
 

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -44,6 +44,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Process;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -55,6 +56,8 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+
+import com.android.internal.util.simpleaosp.QuietHoursHelper;
 
 import com.android.systemui.R;
 import com.android.systemui.screenshot.DeleteScreenshot;
@@ -531,7 +534,9 @@ class GlobalScreenshot {
             @Override
             public void run() {
                 // Play the shutter sound to notify that we've taken a screenshot
-                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                if (!QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                }
 
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotView.buildLayer();

services/java/com/android/server/DockObserver.java
@@ -36,6 +36,8 @@ import android.util.Slog;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import com.android.internal.util.simpleaosp.QuietHoursHelper;
+
 /**
  * <p>DockObserver monitors for a docking station.
  */
@@ -147,8 +149,9 @@ final class DockObserver extends UEventObserver {
 
             // Play a sound to provide feedback to confirm dock connection.
             // Particularly useful for flaky contact pins...
-            if (Settings.Global.getInt(cr,
-                    Settings.Global.DOCK_SOUNDS_ENABLED, 1) == 1) {
+            if ((Settings.Global.getInt(cr,
+                    Settings.Global.DOCK_SOUNDS_ENABLED, 1) == 1)
+                   && !QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
                 String whichSound = null;
                 if (mDockState == Intent.EXTRA_DOCK_STATE_UNDOCKED) {
                     if ((mPreviousDockState == Intent.EXTRA_DOCK_STATE_DESK) ||