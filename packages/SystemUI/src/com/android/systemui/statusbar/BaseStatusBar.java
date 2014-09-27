db242cd14cc63fceb736e4b432859a6186513600
David96 <xD_e-mail@gmx.de>
2014-09-27 15:56:33 +0200

    Hover: open app in fullscreen on longclick
    As requested in AOSPA-793
    
    Question to discuss: is there a better way than a longpress?
    
    Change-Id: I0d8dc82e22717641e93fc279206635bd9de31f6d
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationHelper.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1199,7 +1199,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         return new NotificationClicker(intent);
     }
 
-    public class NotificationClicker implements View.OnClickListener {
+    public class NotificationClicker implements View.OnClickListener, View.OnLongClickListener {
         private KeyguardTouchDelegate mKeyguard;
         private PendingIntent mPendingIntent;
         private Intent mIntent;
@@ -1227,6 +1227,12 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         public void makeFloating(boolean floating) {
             mFloat = floating;
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            onClick(v);
+            return true;
         }
 
         public void onClick(View v) {
